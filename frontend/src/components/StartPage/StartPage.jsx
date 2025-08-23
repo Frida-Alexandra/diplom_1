2cb5705ca88e5fcd1f620a84994fbf811dd9fb24
Frida Alexandra Bogacheva <agirre_miron@mail.ru>
2025-08-23 19:35:57 +0300

    edit

--- frontend/src/components/StartPage/StartPage.jsx
+++ frontend/src/components/StartPage/StartPage.jsx
@@ -21,16 +21,16 @@ function StartPage() {
     return (
         !sessionId
             ? (
-                <section className="start-page">
-                    <div className="start-page--welcome">
-                        <h1 className="start-page--welcome--title"> Хранилице под надзором котиков.</h1>
-                        <h2 className="start-page--welcome--subtitle"> Мяу! </h2>
-                        <div className="start-page--welcome--content">
+                <section class="start-page">
+                    <div class="start-page--welcome">
+                        <h1 class="start-page--welcome--title"> Хранилице под надзором котиков.</h1>
+                        <h2 class="start-page--welcome--subtitle"> Мяу! </h2>
+                        <div class="start-page--welcome--content">
                             Cloud_Server - Кошачье хранилище файлов.
                         </div>
-                        <button className="sing-up-button" onClick={onClickHandler} type="button">Начать</button>
+                        <button class="sing-up-button" onClick={onClickHandler} type="button">Начать</button>
                     </div>
-                    <img className="start-page--image" src={img} alt="StartPage" />
+                    <img src="frontend\src\components\StartPage\StartPage.svg" class="start-page--image" alt="StartPage" />
                 </section>
             )
             : null
